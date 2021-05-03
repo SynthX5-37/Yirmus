0e7965d53ef26d31c2730f96031fd61146d73321
Glitch (glitch-hello-node) <none>
2021-05-03 02:35:51 +0000

    🏌🎉 Checkpoint ./server.js:31494333/132

--- server.js
+++ server.js
@@ -2,5 +2,10 @@ let Discord = require("discord.js")
 let client = new Discord.Client()
 
 client.on("message", message=> {
-  if(message.content === "Yirm")
+  if(message.content === "yirm") {
+    message.channel.send("Yirm")
+}
 })
+
+client.login("ODM4NTkyMTQ4NjAwNjUxNzc2.YI9VzA.KFAc0XnAeSTdCRQfHVORrOHh5G0")
+
