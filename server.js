6065f5a5ce2449138511b62b4261e1e746411480
Glitch (glitch-hello-node) <none>
2021-05-03 02:59:51 +0000

    🤗🚧 Checkpoint ./server.js:31494333/484

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ let client = new Discord.Client()
 
 client.on("message", message=> {
   if(message.content === "yirm") {
-    message.channel.send("Yirm")
+    message.channel.send("Yirm!")
 }
 })
 
 client.login("ODM4NTkyMTQ4NjAwNjUxNzc2.YI9VzA.KFAc0XnAeSTdCRQfHVORrOHh5G0")
 
 client.on("message", message=> {
-  if(message.content === "nigger") {
-    message.channel.send("Fuck you faggot")
+  if(message.content === "Yirm") {
+    message.channel.send("Yirm!")
 }
 })
