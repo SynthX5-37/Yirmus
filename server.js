b01421fc61fd8c1f7ee8e59a60511c8bd607f6db
Glitch (glitch-hello-node) <none>
2021-05-03 03:35:51 +0000

    🎇👋 Checkpoint ./server.js:31494333/463

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 let Discord = require("discord.js")
 let client = new Discord.Client()
 
+client.on("ready", () => {
+  client.user.setPresence({ activity: { name: "with myself"} })
+  
+})
+
 client.on("message", message=> {
   if(message.content === "yirm") {
     message.channel.send("Yirm!")
 }
+client.on("message", message=> {
+  if(message.content === "Yirm") {
+    message.channel.send("Yirm!")
+}
+})
   if(message.content === "YYhelp") {
   let embed = new Discord.MessageEmbed()
   .setTitle("Command List")
@@ -16,10 +26,4 @@ client.on("message", message=> {
 })
 
 client.login("ODM4NTkyMTQ4NjAwNjUxNzc2.YI9VzA.KFAc0XnAeSTdCRQfHVORrOHh5G0")
-
-client.on("message", message=> {
-  if(message.content === "Yirm") {
-    message.channel.send("Yirm!")
-}
-})
 
