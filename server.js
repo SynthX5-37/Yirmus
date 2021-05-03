8a5ff16af0e04da9458a10a7bb41ff9104794a8c
Glitch (glitch-hello-node) <none>
2021-05-03 16:00:42 +0000

    🏢🐲 Checkpoint ./server.js:31494333/64

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ client.on("message", message=> {
   message.channel.send(embed)
   }
   if(message.content === "?sendimage") {
-    let image = new Discord.MessageAttachment(https://tenor.com/view/anime-happy-dance-cat-girl-gif-15501230)
+    let image = new Discord.MessageAttachment
     message.channel.send(image)
 }
 });
