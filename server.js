ab748bb8477287c4bb819c783924aecafe257751
Glitch (yirmus) <none>
2022-05-12 22:59:09 +0000

    🌝🐃 Checkpoint ./server.js:31494333/289

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 let Discord = require("discord.js")
-let client = new Discord.Client(ODM4NTkyMTQ4NjAwNjUxNzc2.YI9VzA.1xdbBGdC4DRXGud5BbyEzg3u8vc)
+let client = new Discord.Client()
 
 client.on("ready", () => {
   client.user.setPresence({ activity: { name: "YYhelp"} })
   
 })
+
+client.login("ODM4NTkyMTQ4NjAwNjUxNzc2.GeQE9g.5OZpnj_e4mECDWxH9UtL396gIn7XwbW7h72QKg")
 
 client.on("message", message=> {
   if(message.content === "yirm") {
